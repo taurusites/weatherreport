73db5860113330de0354f7e6bfc3c34bbd2ca336
William Correia <william@taurusites.com>
2022-09-08 18:35:29 +0530

    Changed API to use dictionary instead of lists

--- api.py
+++ api.py
@@ -5,8 +5,8 @@ apikey = "5b420cc97b3e8854d34ef3f969298896"  # API key
 url = f'https://api.openweathermap.org/data/2.5/weather?appid=' + f'{apikey}'
 
 def weather_api_lookup(list_of_cities):
-  city_weather_data = []
+  city_weather_data = {'City':'','Weather':{}};
   for city in list_of_cities:
-    city_weather_data.append(requests.get(url + f'&q={city}').json())
+    city_weather_data.update({f"{city}":f"{requests.get(url + f'&q={city}').json()}"})
   return city_weather_data
     
--- app.py
+++ app.py
@@ -7,6 +7,5 @@ def app():
   list_of_cities = inputs.inputs()
   city_weather_data = api.weather_api_lookup(list_of_cities)
   print(city_weather_data)
-
 if __name__ == "__main__":
   app()
